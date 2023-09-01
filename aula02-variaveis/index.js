71c60ed930113a0cb239b67b95b30887c007add7
AdelinoNetto <adelino.mecanica@gmail.com>
2023-08-31 22:15:01 -0300

    criando diretorio das tres primeiras aulas javaScript

---
+++ aula02-variaveis/index.js
@@ -0,0 +1,24 @@
+let firstName = 'Adelino'; // variavel do tipo String
+let lastName = 'Neto' // variavel do tipo String
+let idade = 41; // variavel do tipo numeral
+let sexo = 'M'; // variavel do tipo char
+let itemAvaliado = true; // variavel do tipo boolean
+let nameColor // variavel do tipo undefined, não está definido
+const PI = 3.14; // variavel do tipo constante
+
+console.log(firstName);
+console.log(typeof firstName);
+
+console.log(lastName);
+console.log(typeof lastName);
+
+console.log(idade);
+console.log(typeof idade);
+
+console.log(sexo);
+console.log(itemAvaliado);
+console.log(nameColor);
+console.log(PI);
+
+firstName = null // variavel do tipo null
+console.log(firstName)
---
+++ aula03-introducaoObjeto/index.js
@@ -0,0 +1,18 @@
+/**
+    let itemName = 'Pen'
+    let itemPrice = 3
+    let itemAvailable = true
+    let itemColor = 'red'
+ */
+
+// OBJETO É UM GRUPO DE INFORMAÇÕES DE UMA ÚNICA COISA
+let pen = {
+    itemName: 'Pen',
+    itemPrice: 3,
+    itemAvailable: true,
+    itemColor: 'red'
+}
+
+pen.itemColor = 'black'
+
+console.log(pen)
